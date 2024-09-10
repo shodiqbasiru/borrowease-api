828a426cad65d62091db01dc08be12212b2f12bb
shodiqbasiru <shodiqbasiru@gmail.com>
2024-09-10 15:08:39 +0700

    :tada: Initial Commit

---
+++ src/main/java/com/msfb/borrowease/BorrowEaseApplication.java
@@ -0,0 +1,13 @@
+package com.msfb.borrowease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorrowEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msfb/borrowease/BorrowEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msfb.borrowease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
